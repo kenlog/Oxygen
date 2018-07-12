3ae0d62e657ba633e0662854deca50afffd306c9
Bitshop <lennylouise@gmail.com>
2018-07-12 12:23:59 +0200

    updated folder name and paths

--- Plugin.php
+++ Plugin.php
@@ -1,36 +1,30 @@
 <?php
 
-namespace Kanboard\Plugin\Css;
-
+namespace Kanboard\Plugin\MustCss;
 use Kanboard\Core\Plugin\Base;
 
 class Plugin extends Base
 {
     public function initialize()
     {
-        $this->hook->on("template:layout:css", array("template" => "plugins/Css/must.css"));
+        $this->hook->on("template:layout:css", array("template" => "plugins/MustCss/must.css"));
     }
-
     public function getPluginName()
     {
-        return 'Css';
+        return 'Must CSS';
     }
-
     public function getPluginDescription()
     {
         return t('This plugin add a new stylesheet and override default styles.');
     }
-
     public function getPluginAuthor()
     {
         return 'Valentino Pesce';
     }
-
     public function getPluginVersion()
     {
         return '1.0.0';
     }
-
     public function getPluginHomepage()
     {
         return 'https://github.com/kenlog/must.css';
