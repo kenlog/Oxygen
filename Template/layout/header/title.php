7879551eb5ea657d31add016fc7586dc9335354c
Bitshop <lennylouise@gmail.com>
2018-07-14 17:27:08 +0200

    first commit

---
+++ Template/layout/header/title.php
@@ -0,0 +1,20 @@
+<?php global $themeMustCssConfig; ?>
+<div class="header">
+    <?php if (!isset($themeMustCssConfig['logo'])) : ?>
+        <?= $this->url->link('K<span>B</span>', 'DashboardController', 'show', array(), false, '', t('Dashboard')) ?>
+    <?php else: ?>
+        <?= $this->url->link('<img src="'.$this->url->dir().'plugins/MustCss/Img/'.$themeMustCssConfig['logo'].'" />', 'DashboardController', 'show', array(), false, '', t('Dashboard')) ?>
+    <?php endif ?>
+    <h2>
+        <?php if (! empty($project) && ! empty($task)): ?>
+            <?= $this->url->link($this->text->e($project['name']), 'BoardViewController', 'show', array('project_id' => $project['id'])) ?>
+        <?php else: ?>
+            <?= $this->text->e($title) ?>
+        <?php endif ?>
+        <small>
+            <?php if (! empty($description)): ?>
+                <?= $this->app->tooltipHTML($description) ?>
+            <?php endif ?>
+        </small>
+    </h2>
+</div>
