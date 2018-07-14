42220c9042bff5ddf7f0d042f70f36bd05de4c57
Bitshop <lennylouise@gmail.com>
2018-07-14 17:24:19 +0200

    Replace the KB logo

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+/*******************************************************************/
+/* if you want to change the values */
+/*******************************************************************/
+
+// Replace the KB logo with your own logo
+// Just copy your logo-file in the "Img" directory in the plugin directory
+// The file should be to 50 Pixel is a good height
+// Uncomment the next line. 
+
+$themeMustCssConfig['logo'] = 'brand-logo.png';
